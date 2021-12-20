3770ab04b0786a9ba3f78e37608499e2dc63a6cb
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-12-20 14:06:29 +0100

    done with the planner CRUD

--- src/routes/planner/index.js
+++ src/routes/planner/index.js
@@ -3,13 +3,76 @@ import {Planner} from "../../db/models/index.js"
 
 const plannerRouter = express.Router()
 
+//.1
 plannerRouter.get("/", async (req, res, next) => {
     try {
-        res.send("ok")
+
+     const plan = await Planner.findAll()
+        res.send(plan)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+});
+//.2
+plannerRouter.get("/:id", async (req, res, next) => {
+    try {
+
+     const plan = await Planner.findByPk(req.params.id)
+     if(plan)  {
+         res.send(plan)
+     }else{
+         res.status(404).send("Not found")
+     }
     } catch (error) {
         console.log(error)
         next(error)
     }
 }) 
+//.3
+plannerRouter.post("/", async (req, res, next) => {
+    try {
+
+     const plan = await Planner.create(req.body)  
+         res.send(plan)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}) 
+//.4
+plannerRouter.put("/:id", async (req, res, next) => {
+    try {
+
+     const plan = await Planner.update(req.body,{
+     where  :{id:req.params.id},
+     returning: true
+     })  
+         res.send(plan[1][0]);
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}) 
+//.5
+plannerRouter.delete("/:id", async (req, res, next) => {
+    try {
+     const plan = await Planner.destroy({
+          where: {
+            id: req.params.id,
+          },
+        });
+  
+        if (plan) {
+          res.send("ok");
+        } else {
+          res.status(404).send("not found");
+        }
+      } catch (e) {
+        console.log(e);
+        next(e);
+      }
+    });
+  
 
 export default plannerRouter
